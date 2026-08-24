5ec93c9fd0f35077a73f28f78464a8c53d1cca58
overclock <p.simoes@ba.estudante.senai.br>
2026-08-24 20:30:55 -0300

    A

---
+++ senai-dev-software/MinhaApi/Program.cs
@@ -0,0 +1,48 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
+
+using MinhaApi.Repositories;
+using MinhaApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ✅ Registra o Repository
+builder.Services.AddScoped<
+    IProdutoRepository,
+    ProdutoRepository>();
+
+// ✅ Registra a Service
+builder.Services.AddScoped<
+    IProdutoService,
+    ProdutoService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
---
+++ senai-dev-software/MinhaApi/Services/ProdutoService.cs
@@ -0,0 +1,29 @@
+
+public class ProdutoService : IProdutoService
+{
+  private readonly IProdutoRepository _repo;
+
+  public ProdutoService(IProdutoRepository repo)
+      => _repo = repo;
+
+  public IEnumerable<Produto> GetAll()
+      => _repo.GetAll();
+
+  public Produto? GetById(int id)
+      => _repo.GetById(id);
+
+  public Produto Create(Produto produto)
+  {
+      if (produto.Preco < 0)
+          throw new ArgumentException("Preço inválido");
+      _repo.Add(produto);
+      return produto;
+  }
+
+  public Produto? Update(int id, Produto p)
+  {
+      if (_repo.GetById(id) == null) return null;
+      p.Id = id;
+      _repo.Update(p);
+      return p;
+  }
