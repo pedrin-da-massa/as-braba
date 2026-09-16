a0182e01fa7c0e81618475dbb7005d5ec2b5c5b8
overclock <p.simoes@ba.estudante.senai.br>
2026-09-16 19:31:43 -0300

    a

--- senai-dev-software/MinhaApi/obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
+++ senai-dev-software/MinhaApi/obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa224637b6ffa4f6b8f8a4d838ef8fc4ae240359")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e08a7b41563fd2150d00e14f9b38e0e80839ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
